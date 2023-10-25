a1dd153e1b7a68c79be6a5ce2038a3476056094e
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-10-25 11:05:15 -0400

    Update config.js

--- Lab 10/story_starterfiles/config.js
+++ Lab 10/story_starterfiles/config.js
@@ -10,50 +10,21 @@ var config = {
     theme: 'light',
     use3dTerrain: true, //set true for enabling 3D maps.
     title: 'Public and Affordable Housing Flood Risk',
-    subtitle: 'In the fall of 2012, New York City was hit with hurricane that left tens of thousands of public housing residents without power for days or even weeks. New York City Housing Authority public housing developments were affected. 24,000 government-subsidized apartments and 40,000 rent-stabilized apartments were also affected. With rising sea levels from climate change and the promise of more flooding, which areas and who will likely be most affected? How is affordable housing construction responding to this?',
+    subtitle: '',
     byline: 'By a Digital Storyteller',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
-        {
-            id: 'slug-style-id',
-            alignment: 'left',
-            hidden: false,
-            title: 'Display Title',
-            image: 'images/source.png',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: {
-                center: [-73.96295, 40.65619],
-                zoom: 8.5,
-                pitch: 60,
-                bearing: 0
-            },
-            mapAnimation: 'flyTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
+
         {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Second Title',
+            title: 'Background',
             image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            description: 'In the fall of 2012, New York City was hit with hurricane that left tens of thousands of public housing residents without power for days or even weeks. New York City Housing Authority public housing developments were affected. 24,000 government-subsidized apartments and 40,000 rent-stabilized apartments were also affected. With rising sea levels from climate change and the promise of more flooding, which areas and who will likely be most affected? How is affordable housing construction responding to this?',
             location: {
                 center: [-73.96295, 40.65619],
-                zoom: 8.5,
+                zoom: 11.8,
                 pitch: 60,
                 bearing: -43.2,
                 // flyTo additional controls-
@@ -73,7 +44,7 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'Red Hook Housing',
             image: 'images/source.png',
             description: 'Copy these sections to add to your story.',
             location: {
