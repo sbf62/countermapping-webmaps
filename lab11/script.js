5e484d64094a07aec630c261610dc84fbb3f25f9
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-10-27 10:11:45 -0400

    Update script.js

--- lab11/script.js
+++ lab11/script.js
@@ -98,7 +98,17 @@ config.chapters.forEach((record, idx) => {
         story.innerHTML = record.description;
         chapter.appendChild(story);
     }
-
+if (record.video) {
+    var videoDiv = document.createElement('div');
+    videoDiv.setAttribute('class', 'videoContainer');
+    video =  document.createElement('video');
+    video.controls=true;
+    video.autoplay=true;
+    video.loop=true;
+    video.src = record.video;
+    videoDiv.appendChild(video)
+    chapter.appendChild(videoDiv);
+}
     container.setAttribute('id', record.id);
     container.classList.add('step');
     if (idx === 0) {
