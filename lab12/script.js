bea0f6a2c63ce7a82ecc0357bcae24b63066054e
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-10-30 11:46:24 -0400

    Update script.js

--- lab12/script.js
+++ lab12/script.js
@@ -1,19 +1,34 @@
 
- mapboxgl.accessToken = 'pk.eyJ1Ijoic2JmNjIiLCJhIjoiY2xuMXZhaTZkMDIzczJ3cXdmYWt3dTE2NiJ9.0Bwf-GgUm2QzNybs9scQ6Q';
-  const map = new mapboxgl.Map({
-    container: 'map', // container id
-    style: 'mapbox://styles/sbf62/cloczt3l300t301oz17kg69d8', // replace this with your style URL
-    center: [37.775,-122.462], // The convention for coordinates is  typically [long, lat]
-    zoom: 13,
-    // maxZoom: 22,
-    minZoom: 12
-  });
+mapbox://mapbox://styles/sbf62/cloczt3l300t301oz17kg69d8
 
-    map.on('load',function(){
-      // Give your layer's source a nickname you'll remember easily.
-      map.addSource('sandyLayer',{
-        'type':'vector',
-        'url': 'mapbox://iamwfx.bpjzmy2b'
-      });
-  });
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2JmNjIiLCJhIjoiY2xuMXZhaTZkMDIzczJ3cXdmYWt3dTE2NiJ9.0Bwf-GgUm2QzNybs9scQ6Q';
+
+const map = new mapboxgl.Map({
+container: 'Stella',
+style: 'mapbox://styles/sbf62/cloczt3l300t301oz17kg69d8',
+center:[-122.429, 37.772],
+zoom: 13,
+pitch: 25.49,
+bearing:0.0
+});
+
+map.on('load', function () {
+	map.addSource('resturantSource',{
+		'type':'vector',
+		'url': 'mapbox://sbf62.2p6hdvcm'
+	});
+map.addLayer({
+	'id': 'resturantLayer',
+	'type': 'circle',
+	'source': 'resturantSource',
+	'source-layer' : 'Restaurant_Scores_-_LIVES_Sta-8z5cme',
+	'paint':{
+		'circle-color':'#008F8C'
+	}
+
+})
+
+})
+
+
 
