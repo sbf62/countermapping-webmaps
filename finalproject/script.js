6263f5cfde3a038e5e340576da8944e874033834
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-11-27 09:21:40 -0500

    Update script.js

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,12 +1,10 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2JmNjIiLCJhIjoiY2xuMXZhaTZkMDIzczJ3cXdmYWt3dTE2NiJ9.0Bwf-GgUm2QzNybs9scQ6Q';
 
-// Initialize the map
+
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', // You can use your own Mapbox style
-    center: [-74.5, 40], // Set initial map center
-    zoom: 9 // Set initial map zoom level
+    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', 
+    center: [-74.5, 40], 
+    zoom: 9 
 });
 
-// Add map controls, layers, etc. based on your needs
-// Refer to Mapbox documentation for customization options
