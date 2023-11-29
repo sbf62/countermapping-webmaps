7424ed7aa83f554fb7db9f26f4bad84fde65ff53
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-11-29 14:25:09 -0500

    Update script.js

--- finalproject/script.js
+++ finalproject/script.js
@@ -1,10 +1,36 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic2JmNjIiLCJhIjoiY2xuMXZhaTZkMDIzczJ3cXdmYWt3dTE2NiJ9.0Bwf-GgUm2QzNybs9scQ6Q';
 
-
-const map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', 
-    center: [-74.5, 40], 
-    zoom: 9 
+// Initialize Map 1
+const map1 = new mapboxgl.Map({
+    container: 'map1',
+    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', // Replace with your Mapbox style URL
+    center: [-73.981,40.700], // Set initial map center
+    zoom: 8.75 // Set initial map zoom level
 });
 
+// Initialize Map 2
+const map2 = new mapboxgl.Map({
+    container: 'map2',
+    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', // Replace with your Mapbox style URL
+    center: [4.91,52.373], // Set initial map center
+    zoom: 9.5 // Set initial map zoom level
+});
+
+// Initialize Map 3
+const map3 = new mapboxgl.Map({
+    container: 'map3',
+    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', // Replace with your Mapbox style URL
+    center: [-73.981,40.700], // Set initial map center
+    zoom: 8.75 // Set initial map zoom level
+});
+
+// Initialize Map 4
+const map4 = new mapboxgl.Map({
+    container: 'map4',
+    style: 'mapbox://styles/sbf62/clozx8hk700t701qn92q86i3u', // Replace with your Mapbox style URL
+    center: [4.91,52.373], // Set initial map center
+    zoom: 9.5 // Set initial map zoom level
+});
+
+// Add map controls, layers, etc. based on your needs
+// Refer to Mapbox documentation for customization options
