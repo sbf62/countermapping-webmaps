aa46e2ef25c5ca8466ec27f370e6bc39eefe7865
sbf62 <146207817+sbf62@users.noreply.github.com>
2023-12-03 12:26:00 -0500

    Update config.js

--- finalproject/config.js
+++ finalproject/config.js
@@ -30,7 +30,7 @@ var config = {
             hidden: false,
             index: 0,
             title: 'Homelessness in NYC by Borough',
-            image: 'images/source.png',
+            image: 'images/homelessnyc.png',
             description: 'The substantial difference in homelessness rates between Manhattan and Queens not only serves as a critical gauge of the housing crisis but also profoundly influences individuals\' choices about where to live and shapes the lived experiences of residents in these boroughs. The high incidence of homelessness in Manhattan may discourage prospective residents who are concerned about housing stability and affordability. It creates an environment where finding secure and affordable housing becomes a considerable challenge, influencing the residential preferences of those who prioritize housing security. On the contrary, the lower homelessness rates in Queens may make it a more appealing option for individuals seeking a more stable and affordable living situation. The housing crisis, as reflected in these disparate rates, thus plays a pivotal role in shaping the social fabric of each borough, impacting the day-to-day experiences and decisions of residents. Addressing the housing crisis becomes not only an imperative for mitigating homelessness but also for fostering inclusive and sustainable communities where individuals can access stable housing and build their lives.',
             location: {
                 center: [-73.97615, 40.71901],
@@ -56,8 +56,8 @@ var config = {
             hidden: false,
             index: 1,
             title: 'The Housing Crisis in Amsterdam by District',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            image: 'images/povamst.png',
+            description: 'The disparities in poverty rates across Amsterdam\'s districts play a pivotal role in shaping the residential landscape and the everyday experiences of their inhabitants. Zuidoost, grappling with the highest poverty rates, reflects the pressing socioeconomic challenges faced by its residents, encompassing issues like limited economic opportunities and educational disparities. For those considering where to live, the higher poverty rates in Zuidoost may prompt reflections on the broader implications for community resources, social services, and overall quality of life. On the other hand, the lower poverty rates in Zuid suggest a district with potentially more robust economic infrastructure and social support systems, making it an appealing choice for those who prioritize access to diverse opportunities. The contrast in poverty rates underscores the multifaceted nature of the economic challenges within the city, emphasizing the importance of targeted interventions to address systemic inequalities and create thriving, inclusive communities for all residents.',
             location: {
                 center: [-73.97615, 40.71901],
                 zoom: 10,
@@ -81,8 +81,8 @@ var config = {
             hidden: false,
             index: 2,
             title: 'NYCHA Housing Stock by Borough',
-            image: './path/to/image/source.png',
-            description: 'NYCHA Housing Stock by Borough',
+            image: 'images/NYCHApic.png',
+            description: 'The distribution of NYCHA housing stock reveals a notable pattern across the boroughs, with the Bronx having the highest inventory and Staten Island and Queens with the lowest. This discrepancy in public housing availability reflects the unique demographic and socioeconomic landscapes of each borough. The Bronx, characterized by a dense urban population and historically higher rates of poverty, sees a greater need for public housing, resulting in a larger NYCHA housing stock. Conversely, the lower prevalence of NYCHA housing in Staten Island and Queens may be attributed to factors such as different urban planning priorities, lower population density, or varied economic conditions. Understanding this distribution is crucial for policymakers and advocates to tailor housing policies effectively, ensuring that public housing resources are allocated in alignment with the distinct needs of each borough\'s residents. Prospective residents in the Bronx may find a relatively higher availability of NYCHA housing options, while those in Staten Island and Queens may face more limited choices, emphasizing the importance of addressing the diverse housing needs across New York City\'s boroughs.',
             location: {
               center: [-73.97615, 40.71901],
                 zoom: 9.04,
@@ -108,8 +108,8 @@ var config = {
             hidden: false,
             index: 3,
             title: 'Social Housing Stock by District',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+            image: 'images/socialhousepic.png',
+            description: 'The allocation of social housing in Amsterdam unveils a distinct disparity, with Nieuw-West having the highest inventory and Centrum with the lowest. This variation in social housing distribution mirrors the demographic and urban planning differences between these two districts. Nieuw-West, characterized by a mix of residential and industrial areas, holds a greater concentration of social housing, likely driven by considerations of affordability and accessibility for a diverse population. In contrast, Centrum, known for its historic significance and central location, may prioritize other forms of urban development, resulting in a reduced availability of social housing. Recognizing this disparity is crucial for individuals seeking social housing, as it underscores the importance of navigating housing options based on the specific resources and priorities of each district, impacting the choices available to those in search of affordable and socially inclusive living arrangements.',
             location: {
                 center: [-73.97615, 40.71901],
                 zoom: 10.02,
@@ -134,8 +134,8 @@ var config = {
             hidden: false,
             index: 4,
             title: 'Average Rent Paid for NYCHA Housing by Borough',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+         
+            description: 'The revelation that payments for NYCHA housing rent are the highest in Brooklyn and the lowest in the Bronx, while consistently calculated as 30% of income, carries profound implications for the financial well-being of residents in these boroughs. Despite the standardized percentage model, the higher rent payments in Brooklyn indicate a significant income disparity, potentially placing a heavier financial burden on NYCHA tenants in this borough. This emphasizes the urgent need to address broader economic challenges, such as wage discrepancies or the cost of living, to ensure that even a standardized percentage does not disproportionately impact residents. Conversely, the lower rent payments in the Bronx, while reflective of a more affordable living situation, underscore the need to examine and implement strategies that contribute to income stability and economic empowerment across all boroughs. This data highlights the nuanced nature of housing affordability, necessitating a comprehensive approach that considers both standardized rent calculations and broader socioeconomic factors to promote equitable access to affordable housing throughout New York City.',
             location: {
                 center: [-73.97615, 40.71901],
                 zoom: 9,
@@ -160,8 +160,8 @@ var config = {
             hidden: false,
             index: 5,
             title: 'Average Cost of Social Housing In Amsterdam\'s Districts',
-            image: 'images/source.png',
-            description: 'Copy these sections to add to your story.',
+           
+            description: 'The data revealing that Zuidoost has the highest average rent for social housing, while the West district boasts the lowest in Amsterdam, both calculated as a percentage of income, unveils nuanced considerations beyond rent affordability. Despite the standardized rent calculation, the higher rents in Zuidoost may signal additional challenges for social housing tenants, potentially impacting their ability to navigate other elements of the cost of living, such as finding employment or affording essential amenities. Moving to a district with higher average incomes might pose difficulties for residents in Zuidoost, who may face economic disparities and barriers in accessing job opportunities or managing overall living expenses. On the contrary, the lower average rents in the West district may contribute to a more accessible living situation, potentially attracting residents seeking not only affordable housing but also a neighborhood where economic opportunities and the overall cost of living align more favorably. ',
             location: {
                 center: [-73.97615, 40.71901],
                 zoom: 10.04,
@@ -192,6 +192,7 @@ var config = {
         'Income: $30,000 annually.',
         'Current housing situation: Alex currently lives in a small rented apartment. However, the high cost of living in the city, combined with her modest income, has made it increasingly challenging for her to afford rent and other basic necessities. Alex is determined to find more affordable housing options to ease her financial burden. Alex is seeking stable and affordable housing that will allow her to save money, improve her overall financial situation, and focus on her personal and professional growth.',
         'Alex has been searching for affordable housing alternatives, and she has identified NYCHA (New York City Housing Authority) as a potential option due to its subsidized rates for low-income individuals. She understands that the application process may have specific eligibility criteria, and she is prepared to provide all necessary documentation to prove her income level and housing need. She also has interest in social housing in Amsterdam. Amsterdam\'s cost of living may be comparatively lower than New York City, allowing Alex to stretch her income further and save more effectively. Also, the social housing system in Amsterdam is specifically desif=gned designed to ensure a decent quality of life for residents, aligning with Alex’s goals of achieving stability and well-being.',
+        'For someone, like Alex, prioritizing a balanced consideration of these affordable housing factors and holding all else equal, the West district in Amsterdam and the Bronx borough in New York City present compelling options. In both locations, the average rent for social housing is relatively lower, indicating a degree of affordability within the standardized percentage of income model. Additionally, these districts offer the prospect of living in areas with comparatively lower housing costs while potentially benefiting from broader economic opportunities and a more accessible overall cost of living. The West district in Amsterdam and the Bronx borough in NYC, therefore, appeal to individuals seeking a harmonious balance between affordable housing, economic prospects, and the ability to manage other essential living expenses, reflecting a comprehensive approach to addressing the complex nature of housing challenges in diverse urban contexts.',
     ],
     location: {
         center: [-73.97615, 40.71901],
